5c419becf7703214ff95cd6af89224f236cb27a0
Hammad <hammad@pop-os.localdomain>
2023-07-03 01:09:12 +0200

    Add content to home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './home.module.css';
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Home</h1>
-    </main>
-  )
+    return (
+        <main>
+            <section className={styles.hero}>
+                <div className={styles.text}>
+                    <h1 className={styles.title}>Better design for your digital products.</h1>
+                    <p className={styles.description}>
+                        Turning your Idea into Reality. We bring together the teams from the global tech industry.
+                    </p>
+                </div>
+                <Image src='/hero.png' alt='illustration of people' width={500} height={500} />
+            </section>
+        </main>
+    );
 }
